379b81382886c1919168d20c69633dd745f9de43
mayank.gupta <mayank.gupta@programming.com>
2025-09-26 17:35:03 +0530

    push code

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const interBold = Inter({ subsets: ["latin"], weight: "700" });
+export const lusitana = Lusitana({
+    subsets: ["latin"],
+    weight: "400"
+});
